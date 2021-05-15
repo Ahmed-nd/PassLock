65fa958047d2a562beec3130603b24247f7e538f
Ahmed Nasser <62723180+Ahmed-nd@users.noreply.github.com>
2021-05-15 15:36:19 +0200

    encryption add function

--- Passlock app.py
+++ Passlock app.py
@@ -1,9 +1,17 @@
-# Import module 
+# tkinter/ ttk/ webbrowser
 import tkinter as tk
 import webbrowser
 from tkinter import filedialog, Menu
 from tkinter import messagebox
 from tkinter import ttk
+# AES 256 encryption/decryption using pycryptodome library
+# pip install pycryptodomex
+# pip install pycryptodome
+from base64 import b64encode, b64decode
+import hashlib
+from Cryptodome.Cipher import AES
+import os
+from Cryptodome.Random import get_random_bytes
 
 
 class Window(tk.Frame):
@@ -247,6 +255,45 @@ class Window(tk.Frame):
     def open_url(self):
         print("Open Url : " + self)
         webbrowser.open_new(self)
+
+    def encrypt(self, password):
+        # generate a random salt
+        salt = get_random_bytes(AES.block_size)
+
+        # use the Scrypt KDF to get a private key from the password
+        private_key = hashlib.scrypt(
+            password.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+
+        # create cipher config
+        cipher_config = AES.new(private_key, AES.MODE_GCM)
+
+        # return a dictionary with the encrypted text
+        cipher_text, tag = cipher_config.encrypt_and_digest(bytes(self, 'utf-8'))
+        return {
+            'cipher_text': b64encode(cipher_text).decode('utf-8'),
+            'salt': b64encode(salt).decode('utf-8'),
+            'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
+            'tag': b64encode(tag).decode('utf-8')
+        }
+
+    def decrypt(self, password):
+        # decode the dictionary entries from base64
+        salt = b64decode(self['salt'])
+        cipher_text = b64decode(self['cipher_text'])
+        nonce = b64decode(self['nonce'])
+        tag = b64decode(self['tag'])
+
+        # generate the private key from the password and salt
+        private_key = hashlib.scrypt(
+            password.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+
+        # create the cipher config
+        cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
+
+        # decrypt the cipher text
+        decrypted = cipher.decrypt_and_verify(cipher_text, tag)
+
+        return decrypted
 
 
 if __name__ == "__main__":
